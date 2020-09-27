cf23d3a57b98b3ed4c1b850c2b89085ce208a182
IvanDyachuk <ivan4040b@gmail.com>
2020-09-27 10:08:00 +0300

    fix some shit

--- main.py
+++ main.py
@@ -1,48 +1,36 @@
-primer = input('Введите пример').replace(' ', '')
-primer2 = ''
-action = ''
+primer = input('Введите пример: ').replace(' ', '')
+result = ''
 if '+' in primer:
-    action = '+'
     primer2 = primer.replace('+', '')
     if primer2.isdigit():
         primer2 = primer.split('+')
-        Num1 = primer2[0]
-        Num2 = primer2[1]
-        print(int(Num1)+int(Num2))
+        result = int(primer2[0])+int(primer2[1])
     else:
-        print('В примере есть ошибка')
+        result = 'В примере есть ошибка'
 elif '-' in primer:
-    action = '-'
     primer2 = primer.replace('-', '')
     if primer2.isdigit():
         primer2 = primer.split('-')
-        Num1 = primer2[0]
-        Num2 = primer2[1]
-        print(int(Num1)-int(Num2))
+        result = int(primer2[0])-int(primer2[1])
     else:
-        print('В примере есть ошибка')
+        result = 'В примере есть ошибка'
 elif '*' in primer:
-    action = '*'
     primer2 = primer.replace('*', '')
     if primer2.isdigit():
         primer2 = primer.split('*')
-        Num1 = primer2[0]
-        Num2 = primer2[1]
-        print(int(Num1)*int(Num2))
+        result = int(primer2[0])*int(primer2[1])
     else:
-        print('В примере есть ошибка')
+        result = 'В примере есть ошибка'
 elif '/' in primer:
-    action = '/'
     primer2 = primer.replace('/', '')
 
     if primer2.isdigit():
         primer2 = primer.split('/')
-        Num1 = primer2[0]
-        Num2 = primer2[1]
-        if int(Num2) == 0:
-            print('На ноль делить нельзя')
+        if int(primer2[1]) == 0:
+            result = 'На ноль делить нельзя'
         else:
-
-            print(int(Num1)/int(Num2))
+            result = int(primer2[0])/int(primer2[1])
     else:
-        print('В примере есть ошибка')
+        result = 'В примере есть ошибка'
+
+print(result)
